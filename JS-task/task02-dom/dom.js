f601c49a66503faa851d0637b2d77d92c7770b49
Bartha Csaba <csabartha@gmail.com>
2021-04-30 12:54:36 +0200

    commit

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,20 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    document.querySelectorAll("nav#link-list a")
+    .forEach(a => {
+        if (a.innerHTML.includes('outer-link')) {
+        a.setAttribute('target', '_blank');
+        a.innerHTML = '<strong>' + a.innerHTML + '</strong>';
+    }
+})
+
+const nav = document.querySelector("nav");
+nav.style.border = "1px solid blue";
+nav.style.display = "flex";
+nav.style.flexDirection = "column";
+nav.style.margin = "0 auto";
+nav.style.padding = "16px";
+nav.style.width= "30%";
+}
 
 
 export { changeOuterLinks };
