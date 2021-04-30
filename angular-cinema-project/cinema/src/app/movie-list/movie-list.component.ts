1c5d92bdcd059da3a9eb257a51ae487b32312117
Bartha Csaba <csabartha@gmail.com>
2021-04-30 12:53:39 +0200

    commit

--- angular-cinema-project/cinema/src/app/movie-list/movie-list.component.ts
+++ angular-cinema-project/cinema/src/app/movie-list/movie-list.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../model/movie';
-
+import { HttpService } from '../service/http.service';
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.scss']
 })
 export class MovieListComponent implements OnInit {
-
-  movies: Movie[] = [];
-
-  constructor() { }
-
+  movies: Movie[] = []
+  constructor(private movieSerivce:HttpService) { }
   ngOnInit(): void {
   }
-
-  getMovies(): void {}
-
-  deleteMovie(id: number): any {}
-
+  getMovies(): void {
+    this.movieSerivce.getMovieList().subscribe(items=>{
+      this.movies=items;
+    })
+  }
+  deleteMovie(id: number): any {
+    this.movieSerivce.deleteMovie(id);
+    this.getMovies();
+  }
 }
